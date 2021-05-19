2c19da889daa1647cb18aca2e005746822612724
Rafael Caro Repetto <rafael.caro-repetto@kug.ac.at>
2021-05-19 23:53:26 +0200

    Corrected paths for tsv files

--- scripts/carnatic_pattern_family.js
+++ scripts/carnatic_pattern_family.js
@@ -1,8 +1,8 @@
 var target_id = 21186;
 var secondsFactor = 0.0029024875016400026;
 var importance_threshold = 0.4;
-var dataFile = "/files/kamakshi_hierarchy.tsv";
-var pitchFile = "/files/kamakshi_pitch.tsv";
+var dataFile = "files/kamakshi_hierarchy.tsv";
+var pitchFile = "files/kamakshi_pitch.tsv";
 var trackFile = "tracks/Sanjay Subrahmanyan - Kamakshi.mp3";
 
 var data_raw;
